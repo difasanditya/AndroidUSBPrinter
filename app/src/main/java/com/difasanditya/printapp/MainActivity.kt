86e1e7e036dd6f5e0142c268c1142d52f058ee19
Difa Sanditya <difasanditya@gmail.com>
2019-02-26 19:41:30 +0700

    Added Some New Function to Implement Later
    Added all of the available requests for printer device to be implemented later. Also added FlexboxLayout.

--- app/src/main/java/com/difasanditya/printapp/MainActivity.kt
+++ app/src/main/java/com/difasanditya/printapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.difasanditya.printapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,19 +12,84 @@ class MainActivity : AppCompatActivity() {
         val printer = USBAdapter()
         val msg = "This is a message to print"
 
-        val buttonConnect = findViewById<Button>(R.id.connect)
-        buttonConnect.setOnClickListener {
-            printer.createConn(this)
-        }
-
-        val buttonPrint = findViewById<Button>(R.id.print)
+        val buttonPrint = findViewById<Button>(R.id.Print)
         buttonPrint.setOnClickListener {
-            printer.printMessage(msg)
+            printer.print(msg)
         }
 
-        val buttonCancel = findViewById<Button>(R.id.cancel)
-        buttonCancel.setOnClickListener {
-            printer.cancelPrint()
+        /*
+        Printer Connection
+         */
+
+        findViewById<Button>(R.id.connect).setOnClickListener {
+            printer.connect(this)
+        }
+
+        findViewById<Button>(R.id.disconnect).setOnClickListener {
+            printer.disconnect()
+        }
+
+        /*
+        Standard Device Requests
+         */
+
+        findViewById<Button>(R.id.clearFeature).setOnClickListener {
+            printer.clearFeature()
+        }
+
+        findViewById<Button>(R.id.getConfiguration).setOnClickListener {
+            printer.getConfiguration()
+        }
+
+        findViewById<Button>(R.id.getDescriptor).setOnClickListener {
+            printer.getDescriptor()
+        }
+
+        findViewById<Button>(R.id.getInterface).setOnClickListener {
+            printer.getInterface()
+        }
+
+        findViewById<Button>(R.id.getStatus).setOnClickListener {
+            printer.getStatus()
+        }
+
+        findViewById<Button>(R.id.setAddress).setOnClickListener {
+            printer.setAddress()
+        }
+
+        findViewById<Button>(R.id.setConfiguration).setOnClickListener {
+            printer.setConfiguration()
+        }
+
+        findViewById<Button>(R.id.setDescriptor).setOnClickListener {
+            printer.setDescriptor()
+        }
+
+        findViewById<Button>(R.id.setInterface).setOnClickListener {
+            printer.setInterface()
+        }
+
+        findViewById<Button>(R.id.setFeature).setOnClickListener {
+            printer.setFeature()
+        }
+
+        /*
+        Class Specific Requests
+         */
+
+        val textDeviceID = findViewById<TextView>(R.id.textDeviceID)
+
+        findViewById<Button>(R.id.getDeviceID).setOnClickListener {
+            printer.getDeviceID(textDeviceID)
+        }
+
+        val textPortStatus = findViewById<TextView>(R.id.textPortStatus)
+        findViewById<Button>(R.id.getPortStatus).setOnClickListener {
+            printer.getPortStatus(textPortStatus)
+        }
+
+        findViewById<Button>(R.id.softReset).setOnClickListener {
+            printer.softReset()
         }
     }
 }
--- app/src/main/java/com/difasanditya/printapp/USBAdapter.kt
+++ app/src/main/java/com/difasanditya/printapp/USBAdapter.kt
@@ -4,9 +4,10 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.hardware.usb.*
+import android.widget.TextView
 
 class USBAdapter {
-    private val ACTION_USB_PERMISSION = "com.difasanditya.printapp.USB_PERMISSION"
+    private val ACTION_USB_PERMISSION = "com.difasanditya.usbprinter.USB_PERMISSION"
     private var usbManager: UsbManager? = null
     private var printerInterface: UsbInterface? = null
     private var writeEndpoint: UsbEndpoint? = null
@@ -14,9 +15,13 @@ class USBAdapter {
     private var printer: UsbDevice? = null
     private val forceClaim = true
 
-    fun createConn(context: Context) {
-        val usbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
-        val deviceList: HashMap<String, UsbDevice> = usbManager.deviceList
+    /*
+    Printer Connection
+     */
+
+    fun connect(context: Context) {
+        val manager = context.getSystemService(Context.USB_SERVICE) as UsbManager
+        val deviceList: HashMap<String, UsbDevice> = manager.deviceList
         deviceList.values.forEach{ device ->
             if(device.deviceClass == UsbConstants.USB_CLASS_PER_INTERFACE) {
                 deviceLoop@for(i in 0 until device.interfaceCount) {
@@ -30,23 +35,125 @@ class USBAdapter {
                                 readEndpoint = usbInt.getEndpoint(j)
                             }
                         }
-                        if(!usbManager.hasPermission(device)) {
-                            usbManager.requestPermission(device, PendingIntent.getBroadcast(context, 0, Intent(ACTION_USB_PERMISSION), 0))
+                        if(!manager.hasPermission(device)) {
+                            manager.requestPermission(device, PendingIntent.getBroadcast(context, 0, Intent(ACTION_USB_PERMISSION), 0))
                         }
                         printerInterface = usbInt
                         printer = device
-                        this.usbManager = usbManager
+                        usbManager = manager
                     }
                 }
             }
         }
     }
 
-    fun printMessage(msg: String) {
+    /*
+    TODO: Implement the function
+     */
+    fun disconnect() {}
+
+    /*
+    Printing
+     */
+
+    /*
+    Print text from a String
+     */
+    fun print(text: String) {
         if (printer == null) return
         usbManager!!.openDevice(printer)?.apply {
             claimInterface(printerInterface, forceClaim)
-            val b = bulkTransfer(writeEndpoint, msg.toByteArray(), msg.toByteArray().size, 60000)
+            bulkTransfer(writeEndpoint, text.toByteArray(), text.toByteArray().size, 60000)
+            releaseInterface(printerInterface)
+        }
+    }
+
+    /*
+    TODO: Implement the function
+     */
+    fun print(arr: ByteArray) {}
+
+    /*
+    Standard Device Requests
+     */
+
+    /*
+    TODO: Implement the function
+     */
+    fun clearFeature() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun getConfiguration() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun getDescriptor() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun getInterface() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun getStatus() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun setAddress() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun setConfiguration() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun setDescriptor() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun setInterface() {}
+
+    /*
+    TODO: Implement the function
+     */
+    fun setFeature() {}
+
+    /*
+    Class Specific Requests
+     */
+
+    fun getDeviceID(textView: TextView) {
+        if (printer == null) return
+        usbManager!!.openDevice(printer)?.apply {
+            claimInterface(printerInterface, forceClaim)
+            val buffer = ByteArray(255)
+            val len = controlTransfer(0xA1, 0, 0, 0, buffer, 0xFF, 0)
+            var str = "Device ID:"
+            String(buffer, 2, len - 2).split(";").map {
+                str = str.plus("\n" + it)
+            }
+            textView.text = str
+            releaseInterface(printerInterface)
+        }
+    }
+
+    fun getPortStatus(textView: TextView) {
+        if (printer == null) return
+        usbManager!!.openDevice(printer)?.apply {
+            claimInterface(printerInterface, forceClaim)
+            val buffer = ByteArray(255)
+            controlTransfer(0xA1, 1, 0, 0, buffer, 1, 0)
+            var str = "Port Status: " + String(buffer) + "\n"
+            textView.text = str
             releaseInterface(printerInterface)
         }
     }
@@ -56,11 +163,11 @@ class USBAdapter {
     https://www.usb.org/sites/default/files/usbprint11a021811.pdf (Class-Specific Requests, Page 6)
     http://sdphca.ucsd.edu/lab_equip_manuals/usb_20.pdf (Chapter 9: USB Device Framework, Page 239)
      */
-    fun cancelPrint() {
+    fun softReset() {
         if (printer == null) return
         usbManager!!.openDevice(printer)?.apply {
             claimInterface(printerInterface, forceClaim)
-            val c = controlTransfer(0x21, 2, 0, 0, null, 0, 0)
+            controlTransfer(0x21, 2, 0, 0, null, 0, 0)
             releaseInterface(printerInterface)
         }
     }
